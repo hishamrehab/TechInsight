6e204d82340da127935970fe0761996360b34600
hishamrehab <hishamrehab30@gmail.com>
2026-04-15 13:12:22 +0200

    Adding Scroll to top component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Navbar } from '../components/shared/Navbar'
+import { ScrollToTop } from '../components/shared/ScrollToTop'
 import { SiteFooter } from '../components/shared/SiteFooter'
 
 export function MainLayout() {
   return (
     <div className="app-shell min-h-screen bg-slate-900 text-white">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <SiteFooter />
